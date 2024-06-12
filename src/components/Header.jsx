803fe0f4152a703bc942ea41709ec74ccbf99709
hassam-01 <ali.hassam1@outlook.com>
2024-06-12 23:04:06 +0500

    header completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { MdLocationPin } from "react-icons/md";
+import { FaShoppingBasket } from "react-icons/fa";
+import { LuArrowDownCircle } from "react-icons/lu";
+
+import star from "../assets/star.png";
+
+export default function Header() {
+  return (
+    <div className="md:flex justify-between hidden md:bg-[#edededc8] md:pl-2 md:mx-28 md:rounded-b-2xl">
+      <div className="flex justify-between items-center space-x-2 ">
+        <img src={star} alt="" className="h-6 w-6" />
+        <p className="font-medium">
+          Get 5% Off your first order,
+          <span className="text-[#FC8A06] font-semibold"> Promo: ORDER5</span>
+        </p>
+      </div>
+      <div className="flex justify-between space-x-5 items-stretch">
+        <div className="flex justify-between items-center space-x-3 font-medium">
+          <MdLocationPin className="h-6 w-6 " />
+          <p className="text-[#03081F]">Regent Street, A4, A4201, London</p>
+          <a href="location" className="text-[#FC8A06] hover:border-b">
+            Change Location
+          </a>
+        </div>
+        <div className="flex justify-between bg-[#028643] rounded-b-2xl text-white items-cente space-x-3  h-ful items-stretch">
+          <div className="pr-3 pl-2 border-r flex items-center py-2">
+            <FaShoppingBasket className="h-8 w-8" />
+          </div>
+          <div className="pr-3 pl-2  border-r flex items-center py-2">
+            <p>23</p>
+          </div>
+          <div className="pr-3 pl-2 border-r flex items-center py-2">
+            <p>Rs.100</p>
+          </div>
+          <div className="pr-3 pl-2 h-full flex items-center py-2">
+            <LuArrowDownCircle className="h-8 w-8" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="bg-red-500 text-black">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis, explicabo.
+      </div>
+    </div>
+  )
+}
