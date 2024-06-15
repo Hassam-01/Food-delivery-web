f5d12963a5887c4a643f03ad8d2efa679e5077ce
hassam-01 <ali.hassam1@outlook.com>
2024-06-16 01:34:56 +0500

    popular restaurants added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Banner from './shared/Banner'
 import Categories from './shared/Categories'
 import Deal from './shared/Deal'
+import Popular from './shared/Popular'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Banner text2_1={"Feast Your Senses,"} text2_2={"Fast and Fresh"} text1={"Order Restaurant food, takeaway and groceries."} text3={"Enter a postcode to see what we deliver"} bgColor={"bg-black"}/>
       <Deal/>
       <Categories/>
+      <Popular/>
     </div>
   )
 }
